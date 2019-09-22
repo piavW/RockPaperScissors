45512db361de714da37dcd247a178cf229c12ab9
piavw <piavonwachenfelt@hotmail.com>
2019-09-22 16:33:28 +0200

    Adds unittest for computer choice. New test isn't passing - no implementatincode is written

--- spec/app_spec.js
+++ spec/app_spec.js
@@ -3,6 +3,7 @@ require('../spec.helper')
 describe('Rock Paper Scissor basic logic', () => {
     let game = new Game
 
+
     it('paper beats rock', () => {
         expect(game.check(1, 0)).to.eq("Paper wins")
     })
@@ -15,4 +16,18 @@ describe('Rock Paper Scissor basic logic', () => {
     it('if both choices are the same, the game results in a tie', () => {
         expect(game.check(1,1)).to.eq("Tied, try again!")
     })
+        describe('Computer enters the game', () => {
+            const rock = 0
+            const paper = 1
+            const scissor = 2
+            let optArray = [rock, paper, scissor]
+            let randomIndex = Math.floor(Math.random()*optArray.length);
+            let randomChoice = optArray[randomIndex]
+            
+            
+            it('computer can choose at random from an array', () => {
+                expect(computerChoice.randomChoice()).to.eq(randomChoice())
+            })
+        })
+    
 })
