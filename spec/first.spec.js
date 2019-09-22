59a5bd67478c7f821e6f0d28f01710534b5a76fe
piavw <piavonwachenfelt@hotmail.com>
2019-09-22 13:32:29 +0200

    Modifies function Game, creates passing test for rock vs paper

--- spec/first.spec.js
+++ spec/first.spec.js
@@ -4,11 +4,30 @@ describe('Rock Paper Scissor basic logic', () => {
     const rock = 0
     const paper = 1
     const scissor = 2
+    let game = new Game
 
+    function Game() {
+        this.check = (par1, par2) => {
+            if (par1 == par2) {
+                return "Tied, try again!"
+            } else if (par1==1 && par2==0) { //paper vs rock
+                return "Paper wins"
+            } else if (par1==0 && par2==1) { //paper vs rock
+                return "Paper wins"
+            } else if (par1==0 && par2==2) { //rock vs scissor
+                return "Rock wins"
+            } else if (par1==2 && par2==0) { //rock vs scissor
+                return "Rock wins"
+            } else if (par1==2 && par2==1) { //scissor vs paper
+                return "Scissor wins" 
+            } else if (par1==1 && par2==2) { //scissor vs paper
+                return "Scissor wins" 
+            };
+        }
+    }
 
     it('rock beats scissor', () => {
-        Game(rock, scissor)
-        expect().to.be("Rock wins")
+        expect(game.check(1, 0)).to.eq("Paper wins")
     })
 })
 
